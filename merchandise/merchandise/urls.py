44fbd159c2bdce9f692f98ea4fb8a175db91e977
John Martin <jem2305@gmail.com>
2017-03-16 13:10:08 -0400

    Include item urls

--- merchandise/merchandise/urls.py
+++ merchandise/merchandise/urls.py
@@ -13,13 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from frontend.views import IndexView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^item/',
+        include('item.urls',
+                namespace='item')),
+    url(r'^admin/', 
+        admin.site.urls),
+    url(r'^login/$', 
+        auth_views.login, 
+        name='login'),
+    url(r'^$', 
+        IndexView.as_view(), 
+        name='index'),
 ]
