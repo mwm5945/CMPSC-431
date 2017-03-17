66afe6412d89ab981c519bb5c80115c58f5b6db1
John Martin <jem2305@gmail.com>
2017-03-17 18:02:33 -0400

    Item create view

--- merchandise/item/urls.py
+++ merchandise/item/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
+from django.urls import reverse
 from item import views
 
 app_name = 'item'
 urlpatterns = [
+    url(r'^create/$', 
+        views.ItemDetailsCreateView.as_view(), 
+        name='item_details_create'),
     url(r'^(?P<pk>[-\w]+)/$', 
         views.ItemDetailsDetailView.as_view(), 
         name='item_details_detail'),
--- merchandise/item/views.py
+++ merchandise/item/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse
 from item.models import ItemDetails
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 
 class ItemDetailsListView(ListView):
@@ -28,3 +31,27 @@ class ItemDetailsDetailView(DetailView):
         context = super(ItemDetailsDetailView, self).get_context_data(**kwargs)
         context.update(self.params)
         return context
+
+
+class ItemDetailsCreateView(CreateView):
+    """Create View for ItemDetails."""
+
+    model = ItemDetails
+    fields = ['name', 'description', 'category', 'is_active']
+    params = {
+        'page_header': "New Item"
+    }
+
+    def get_context_data(self, **kwargs):
+        context = super(ItemDetailsCreateView, self).get_context_data(**kwargs)
+        context.update(self.params)
+        return context
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.helper = FormHelper()
+        form.helper.add_input(Submit('submit', 'Submit', css_class='btn-primary'))
+        return form
+    
+    def get_success_url(self):
+        return reverse('item:item_details_detail', kwargs={'pk':self.object.id})
