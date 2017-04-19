6dfa710230e184fa9d1c972aff1754dd0d95f748
John Martin <jem2305@gmail.com>
2017-04-19 17:51:56 -0400

    Order Create/Update

--- merchandise/supply/urls.py
+++ merchandise/supply/urls.py
@@ -3,6 +3,12 @@ from supply import views
 
 app_name = 'order'
 urlpatterns = [
+    url(r'^create/$', 
+        views.OrderCreateView.as_view(), 
+        name='order_create'),
+    url(r'^update/(?P<pk>[-\w]+)/$', 
+        views.OrderUpdateView.as_view(), 
+        name='order_update'),
     url(r'^(?P<pk>[-\w]+)/$',
         views.OrderDetailView.as_view(),
         name='order_detail'),
